f7df656c781de8b9eee59402e2a58b66d66245f4
schebl <shochebl@gmail.com>
2025-05-06 22:33:38 +0300

    add polygon building

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,30 @@
+<script lang="ts">
+    import {Canvas} from "./lib/Canvas";
+    import type {Point} from "./lib/Point";
+
+    let canvas: HTMLCanvasElement;
+
+    const polygonPoints: Point[] = $state([]);
+
+    function handleAddPoint(e: MouseEvent) {
+        polygonPoints.push({
+            x: e.offsetX,
+            y: e.offsetY,
+        });
+    }
+
+    $effect(() => {
+        const cnv = new Canvas(canvas);
+        cnv.drawPoints(polygonPoints, true);
+    });
+</script>
+
 <main>
-    <h1>area</h1>
+    <canvas
+        bind:this={canvas}
+        onclick={handleAddPoint}
+        width="500"
+        height="500"
+        style="border: 1px solid #535bf2"
+    ></canvas>
 </main>
---
+++ src/lib/Canvas.ts
@@ -0,0 +1,34 @@
+import type {Point} from "./Point";
+
+export class Canvas {
+    private readonly ctx: CanvasRenderingContext2D;
+
+    constructor(canvas: HTMLCanvasElement) {
+        const ctx = canvas.getContext("2d");
+        if (!ctx) {
+            throw new Error("unable to get context from canvas");
+        }
+        this.ctx = ctx;
+
+        this.ctx.strokeStyle = "#535bf2";
+    }
+
+    public drawPoints(points: Point[], closePath: boolean) {
+        this.clear();
+
+        this.ctx.beginPath();
+        for (let i = 0; i < points.length; i++) {
+            this.ctx.lineTo(points[i].x, points[i].y);
+        }
+
+        if (closePath) {
+            this.ctx.closePath();
+        }
+
+        this.ctx.stroke();
+    }
+
+    private clear() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+}
