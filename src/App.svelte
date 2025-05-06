af524cb9a189cef5ae1ab065d6cbe7c483375dbf
schebl <shochebl@gmail.com>
2025-05-06 22:56:28 +0300

    add area calculating

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,18 @@
     let canvas: HTMLCanvasElement;
 
     const polygonPoints: Point[] = $state([]);
+    let polygonArea = $derived(getPolygonArea(polygonPoints));
+
+    function getPolygonArea(polygon: Point[]): number {
+        let area = 0;
+        for (let i = 0; i < polygon.length; i++) {
+            const {x: x1, y: y1} = polygon[i];
+            const {x: x2, y: y2} = polygon[(i + 1) % polygon.length];
+
+            area += x1 * y2 - x2 * y1;
+        }
+        return Math.abs(area) / 2;
+    }
 
     function handleAddPoint(e: MouseEvent) {
         polygonPoints.push({
@@ -27,4 +39,6 @@
         height="500"
         style="border: 1px solid #535bf2"
     ></canvas>
+
+    <p>Area: {polygonArea}</p>
 </main>
